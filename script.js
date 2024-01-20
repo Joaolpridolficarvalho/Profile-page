87361d3dcbc5989cc40124d166c4c0da41b2935e
Joaolpridolficarvalho <jlprcarvalho@gmail.com>
2024-01-20 12:00:19 -0300

    Start to create the functions

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+function createInput(){
+    var input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'input');
+    
+    return input;
+}
